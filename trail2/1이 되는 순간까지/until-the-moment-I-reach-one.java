06638a12238594548552d8107756bacd84c7a162
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-05-16 02:43:08 +0900

    [Passed] 1이 되는 순간까지 (84ms, 9MB)

---
+++ trail2/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int total = 0;
+
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        cal(n);
+
+        System.out.print(total);
+    }
+
+    public static int cal(int n){
+        if(n == 1) return 1;
+        total++;
+
+        if(n%2 == 0) {
+            return cal(n/2);
+        }
+        else{
+            return cal(n/3);
+        }
+    }
+}
