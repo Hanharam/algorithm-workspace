42672eb46aca00d3240634496f141b4e677bad1a
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-05-23 00:26:01 +0900

    [Passed] Top K 숫자 구하기 (104ms, 9MB)

---
+++ trail2/Top K 숫자 구하기/kth-number.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken()) - 1;
+
+        int[] nums = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        System.out.print(nums[k]);
+
+
+    }
+}
