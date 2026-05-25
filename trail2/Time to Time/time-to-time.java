902a13c4b64457f44f0734f77f2b62016440b99a
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-05-26 02:33:52 +0900

    [Passed] Time to Time (88ms, 9MB)

--- trail2/Time to Time/time-to-time.java
+++ trail2/Time to Time/time-to-time.java
@@ -6,26 +6,12 @@ public class Main {
         // Please write your code here.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int hours = Integer.parseInt(st.nextToken());
-        int mins = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
         int d = Integer.parseInt(st.nextToken());
-        int elapsedTime = 0;
 
-        while(true) {
-            if(hours == c && mins == d) {
-                break;
-            }
-
-            elapsedTime++;
-            mins++;
-
-            if(mins == 60) {
-                hours++;
-                mins = 0;
-            }
-        }
-
-        System.out.print(elapsedTime);
+        System.out.print((c - a) * 60 + d - b);
+        
     }
 }
