121f16584cc3e783d60e44ca096ceac4f042cd96
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-06-17 15:33:25 +0900

    [Passed] 겹치지 않는 사각형의 넓이 (216ms, 36MB)

--- trail2/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
+++ trail2/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -2,36 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static final int N = 3;
+    public static final int MAX_R = 2000;
+    public static final int OFFSET = 1000;
+
+    public static int[] x1 = new int[N];
+    public static int[] y1 = new int[N];
+    public static int[] x2 = new int[N];
+    public static int[] y2 = new int[N];
+
+
+    public static int[][] checked = new int[MAX_R + 1][MAX_R + 1];
+
     public static void main(String[] args) throws IOException{
         // Please write your code here.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[][] grid = new int[2001][2001];
-
-        for(int i = 0; i < 3; i++) {
+        
+        for(int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+            x1[i] = Integer.parseInt(st.nextToken());
+            y1[i] = Integer.parseInt(st.nextToken());
+            x2[i] = Integer.parseInt(st.nextToken());
+            y2[i] = Integer.parseInt(st.nextToken());
 
-            
-            for(int j = x1; j < x2; j++) {
-                for(int k = y1; k < y2; k++) {
-                    if(i == 2) grid[j+1000][k+1000] += 2;
-                    else grid[j+1000][k+1000]++;
+            x1[i] += OFFSET;
+            y1[i] += OFFSET;
+            x2[i] += OFFSET;
+            y2[i] += OFFSET;
+
+        }
+
+        for(int i = 0; i < N; i++) {
+            for(int x = x1[i]; x < x2[i]; x++) {
+                for(int y = y1[i]; y < y2[i]; y++) {
+                    checked[x][y] += i + 1;
                 }
             }
         }
 
-        int cnt = 0;
-        for(int i = 0; i < 2001; i++) {
-            for(int j = 0; j < 2001; j++) {
-                if(grid[i][j] == 1) {
-                    cnt++;
-                }
+        int area = 0;
+
+        for(int x = 0; x < MAX_R + 1; x++) {
+            for(int y = 0; y < MAX_R + 1; y++) {
+                if(checked[x][y] == 1 || checked[x][y] ==2) area++;
             }
         }
 
-        System.out.print(cnt);
+        System.out.print(area);
     }
 }
