cf641bc8989637215adcc81a42b0b267e7e5668d
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-06-19 23:53:25 +0900

    [Passed] 거울에 레이저 쏘기 2 (93ms, 9MB)

--- trail2/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
+++ trail2/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -6,41 +6,12 @@ public class Main {
         return ( x >= 0 && x < n && y >= 0 && y < n);
     }
 
-    public static int toDirIn(char dir) {
-        if(dir == 'E') return 0;
-        else if(dir == 'S') return 1;
-        else if(dir == 'W') return 2;
-        else return 3;
-    }
-    public static int toDirDe(char dir) {
-        if(dir == 'E') return 0;
-        else if(dir == 'S') return 3;
-        else if(dir == 'W') return 1;
-        else return 2;
-    }
-    public static char toNumIn(int dir) {
-        if(dir == 0) return 'E';
-        else if(dir == 1) return 'S';
-        else if(dir == 2) return 'W';
-        else return 'N';
-    }
-    public static char toNumDe(int dir) {
-        if(dir == 0) return 'E';
-        else if(dir == 1) return 'W';
-        else if(dir == 2) return 'N';
-        else return 'S';
-    }
-
-
     public static void main(String[] args) throws IOException{
         // Please write your code here.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] dxIn = new int[]{0, 1, 0, -1}; // E, S, W, N
-        int[] dyIn = new int[]{1, 0, -1, 0};
-
-        int[] dxDe = new int[]{0, 0, -1, 1}; // E, W, N, S
-        int[] dyDe = new int[]{1, -1, 0, 0};
+        int[] dx = new int[]{1, 0, -1, 0}; // S, W, N, E
+        int[] dy = new int[]{0, -1, 0, 1};
 
         int n = Integer.parseInt(br.readLine());
 
@@ -53,61 +24,40 @@ public class Main {
             }
         }
 
-        char prev;
-        int dir;
+        int num = Integer.parseInt(br.readLine());
+        int moveDir;
+        int x, y;
 
-        int k = Integer.parseInt(br.readLine());
-        int dim = (k - 1) / n;
-
-        // 처음 진입할 방향 구하기
-        if(dim < 1) prev = 'S';
-        else if(dim < 2) prev = 'W';
-        else if(dim < 3) prev = 'N';
-        else prev = 'E';
-
-        // 처음 진입할 위치 구하기
-        int[] dx = new int[]{0, 1, 0, -1}; // E, S, W, N
-        int[] dy = new int[]{1, 0, -1, 0};
-
-        int x = 0;
-        int y = 0;
-        int inputDir = 0;
-
-        for(int i = 0; i < k - 1; i++) {
-            int nx = x + dx[inputDir];
-            int ny = y + dy[inputDir];
-
-            if(inRange(nx, ny, n)) {
-                x = nx;
-                y = ny;
-            }
-            else {
-                inputDir = (inputDir + 1) % 4;
-            }
+        // 초기 방향, 위치
+        if(num <= n) {
+            x = 0; y = num - 1; moveDir = 0;
+        }
+        else if(num <= 2 * n) {
+            x = num - n - 1; y = n - 1; moveDir = 1;
+        }
+        else if(num <= 3 * n) {
+            x = n - 1; y = 3 * n - num; moveDir = 2;
+        }
+        else {
+            x = 4 * n - num; y = 0; moveDir = 3;
         }
 
-        int cnt = 0;
+        int moveNum = 0;
 
         while(inRange(x, y, n)) {
             if(grid[x][y] == '/') {
-                dir = toDirIn(prev); // 이전의 문자열을 거울 형태에 따라서 변경
-                dir = 3 - dir; // 새로운 방향을 숫자 형태로 구함
-                prev = toNumIn(dir); // 숫자 형태의 새로운 방향을 문자형태로 변환 후 전달
-
-                x += dxIn[dir];
-                y += dyIn[dir];
+                moveDir = moveDir ^ 1;
+                x += dx[moveDir];
+                y += dy[moveDir];
             }
             else {
-                dir = toDirDe(prev); // 이전의 문자열을 거울 형태에 따라서 변경
-                dir = 3 - dir; // 새로운 방향을 숫자 형태로 구함
-                prev = toNumDe(dir); // 숫자 형태의 새로운 방향을 문자형태로 변환 후 전달
-
-                x += dxDe[dir];
-                y += dyDe[dir];
+                moveDir = 3 - moveDir;
+                x += dx[moveDir];
+                y += dy[moveDir];
             }
-            cnt++;
+            moveNum++;
         }
 
-        System.out.print(cnt);
+        System.out.print(moveNum);
     }
 }
