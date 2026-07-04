d475ad3a6bfd72048cf88d88a8c027992cba5bca
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-07-04 16:54:57 +0900

    [Passed] 수열 조작 (169ms, 13MB)

---
+++ trail3/수열 조작/sequence-manipulation.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for(int i = 1; i <= n; i++) {
+            dq.addLast(i);
+        }
+
+        while(dq.size() > 1) {
+            dq.pollFirst();
+            dq.addLast(dq.peekFirst());
+            dq.pollFirst();
+        }
+
+        System.out.print(dq.pollFirst());
+    }
+}
