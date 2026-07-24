7913ff2e575d3621a1f224466ae2bd9ab802bc26
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-07-24 17:36:05 +0900

    [Passed] 크기가 N인 순열 (260ms, 24MB)

---
+++ trail4/크기가 N인 순열/n-permutation.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static StringBuilder sb = new StringBuilder();
+    public static ArrayList<Integer> ans = new ArrayList<>();
+
+    public static boolean[] visited = new boolean[9];
+
+    public static void printArray() {
+        for(int i = 0; i < n; i++) {
+            sb.append(ans.get(i)).append(" ");
+        }
+        sb.append("\n");
+    }
+
+    public static void choose(int curNum) {
+        if(curNum == n + 1) {
+            printArray();
+            return;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            if(visited[i]) continue;
+
+            ans.add(i);
+            visited[i] = true;
+            choose(curNum + 1);
+            ans.remove(ans.size() - 1);
+            visited[i] = false;
+        }
+        return;
+    }
+
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        choose(1);
+        System.out.print(sb);
+    }
+}
