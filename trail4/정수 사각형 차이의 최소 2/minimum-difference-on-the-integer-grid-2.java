e1d92e45f13b66bcb92e42bea332833123dcf520
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-08-01 23:20:36 +0900

    [Passed] 정수 사각형 차이의 최소 2 (213ms, 10MB)

--- trail4/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
+++ trail4/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -2,15 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static final int INF = Integer.MAX_VALUE;
+
     public static int n;
     public static int[][] grid = new int[100][100];
+    public static int[][] dp = new int[100][100];
 
-    public static int answer = Integer.MAX_VALUE;
+    public static int ans = INF;
 
-    public static boolean inRange(int x, int y) {
-        return 0 <= x && x < n && 0 <= y && y < n;
+    public static void initialize() {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                dp[i][j] = INF;
+            }
+        }
+
+        dp[0][0] = grid[0][0];
+
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], grid[i][0]);
+            dp[0][i] = Math.max(dp[0][i - 1], grid[0][i]);
+        }
     }
 
+    public static int solve(int low) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] < low) {
+                    grid[i][j] = INF;
+                }
+            }
+        }
+
+        initialize();
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = Math.max(
+                    Math.min(dp[i - 1][j], dp[i][j - 1]),
+                    grid[i][j]
+                );
+            }
+        }
+
+        return dp[n - 1][n - 1];
+    }
 
     public static void main(String[] args) throws IOException{
         // Please write your code here.
@@ -23,53 +59,18 @@ public class Main {
                 grid[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
-        int answer = Integer.MAX_VALUE;
 
         for(int low = 1; low <= 100; low++) {
-            int[][] dp = new int[n][n];
+            int upper = solve(low);
 
-            for(int i = 0; i < n; i++) {
-                Arrays.fill(dp[i], Integer.MAX_VALUE);
-            }
-
-            if(grid[0][0] < low) {
+            if(upper == INF) {
                 continue;
             }
 
-            dp[0][0] = grid[0][0];
-
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    if(i == 0 && j == 0) {
-                        continue;
-                    }
-
-                    if(grid[i][j] < low) {
-                        continue;
-                    }
-
-                    int prev = Integer.MAX_VALUE;
-
-                    if(i > 0) {
-                        prev = Math.min(prev, dp[i - 1][j]);
-                    }
-
-                    if(j > 0) {
-                        prev = Math.min(prev, dp[i][j - 1]);
-                    }
-
-                    if(prev != Integer.MAX_VALUE) {
-                        dp[i][j] = Math.max(prev, grid[i][j]);
-                    }
-                }
-            }
-
-            if(dp[n - 1][n - 1] != Integer.MAX_VALUE) {
-                answer = Math.min(answer, dp[n - 1][n - 1] - low);
-            }
+            ans = Math.min(ans, upper - low);
         }
 
-        System.out.print(answer);
+        System.out.print(ans);
+        
     }
 }
