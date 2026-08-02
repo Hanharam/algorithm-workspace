33ab1f8843cb5d18b34c1409ef73a10949daae5b
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-08-02 19:05:44 +0900

    [Passed] 알바로 부자 되기 (89ms, 9MB)

--- trail4/알바로 부자 되기/being-rich-by-working-part-time.java
+++ trail4/알바로 부자 되기/being-rich-by-working-part-time.java
@@ -1,49 +1,96 @@
 import java.util.*;
 import java.io.*;
 
-class Job {
-    int s, e, p;
+// Two Pointer 방식
 
-    public Job(int s, int e, int p) {
-        this.s = s;
-        this.e = e;
-        this.p = p;
+class Pair implements Comparable<Pair> {
+    int x, y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Pair b) {
+        if(x != b.x) return x - b.x;
+        return this.y - b.y;
     }
 }
 
 public class Main {
+    public static final int INT_MIN = Integer.MIN_VALUE;
+    public static final int MAX_NUM = 1000;
+
     public static int n;
-    public static Job[] jobs = new Job[1000];
-    public static int[] dp = new int[1000];
+    public static int[] dp = new int[MAX_NUM + 1];
+
+    public static int[] s = new int[MAX_NUM + 1];
+    public static int[] e = new int[MAX_NUM + 1];
+    public static int[] p = new int[MAX_NUM + 1];
+
+    public static Pair[] sortedWorks = new Pair[MAX_NUM + 1];
+
+    public static void preprocess() {
+        // 알바가 끝나는 순으로 정렬
+        for(int i = 0; i <= n; i++) {
+            sortedWorks[i] = new Pair(e[i], i);
+        }
+
+        Arrays.sort(sortedWorks, 1, n + 1);
+    }
+
+    public static void initialize() {
+        for(int i = 0; i <= n; i++) {
+            dp[i] = INT_MIN;
+        }
+
+        s[0] = e[0] = p[0] = 0;
+        dp[0] = 0;
+    }
+
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 1; i <= n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int p = Integer.parseInt(st.nextToken());
-
-            jobs[i] = new Job(s, e, p);
-            dp[i] = p;
+            s[i] = Integer.parseInt(st.nextToken());
+            e[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j < i; j++) {
+        for(int i = 1; i <= n; i++) {
+            sortedWorks[i] = new Pair(0, 0);
+        }
 
-                if(jobs[i].s > jobs[j].e) {
-                    dp[i] = Math.max(dp[i], dp[j] + jobs[i].p);
-                }
+        initialize();
+
+        preprocess();
+
+        int maxJ = 0;
+
+        int ptr = 1;
+
+        for(int i = 1; i <= n; i++) {
+
+            while(sortedWorks[ptr].x < s[i]) {
+                int j = sortedWorks[ptr].y;
+                if(dp[j] > dp[maxJ])
+                    maxJ = j;
+                
+                ptr++;
             }
+
+            dp[i] = Math.max(dp[i], dp[maxJ] + p[i]);
         }
 
-        int ans = 0;
-        for(int i = 0; i < n; i++) {
-            ans = Math.max(ans, dp[i]);
+        int answer = 0;
+        for(int i = 0; i <= n; i++) {
+            answer = Math.max(answer, dp[i]);
         }
 
-        System.out.print(ans);
+        System.out.println(answer);
     }
 }
