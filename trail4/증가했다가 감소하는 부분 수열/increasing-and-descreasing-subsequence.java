2b530d88f8b330bc639506d8943dd189c55b010b
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-08-02 18:10:06 +0900

    [Passed] 증가했다가 감소하는 부분 수열 (134ms, 10MB)

--- trail4/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.java
+++ trail4/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
     public static int n;
     public static int[] arr = new int[1000];
-    public static int[][] dp = new int[2][1000]; // 0: 증가, 1: 감소, 2: 증가했다 감소 중
+    public static int[][] dp = new int[1000][2];
 
     public static void main(String[] args) throws IOException{
         // Please write your code here.
@@ -16,31 +16,27 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 0; i < 2; i++) {
-            Arrays.fill(dp[i], 1);
-        }
+        for(int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+            dp[i][1] = 1;
 
-        for(int i = 1; i < n; i++) {
             for(int j = 0; j < i; j++) {
-                if(arr[i] > arr[j]) {
-                    dp[0][i] = Math.max(dp[0][i], dp[0][j] + 1);
-                }
-            }
-        }
 
-        for(int i = n - 2; i >= 0; i--) {
-            for(int j = n - 1; j > i; j--) {
-                if(arr[i] > arr[j]) {
-                    dp[1][i] = Math.max(dp[1][i], dp[1][j] + 1);
-                }
+                if(arr[j] < arr[i]) 
+                    dp[i][0] = Math.max(dp[i][0], dp[j][0] + 1);
+
+                if(arr[j] > arr[i])
+                    dp[i][1] = Math.max(dp[i][1], dp[j][1] + 1);
             }
+
+            dp[i][1] = Math.max(dp[i][1], dp[i][0]);
         }
 
         int ans = 0;
         for(int i = 0; i < n; i++) {
-            ans = Math.max(ans, dp[0][i] + dp[1][i]);
+            ans = Math.max(ans, dp[i][1]);
         }
         
-        System.out.print(ans - 1);
+        System.out.print(ans);
     }
 }
