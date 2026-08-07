3d97d9e1ddd72388ec590ae923c8ad4444ec1cfe
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-08-07 20:12:50 +0900

    [Passed] 고대 보물 지도의 비밀 (97ms, 16MB)

--- trail4/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
+++ trail4/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
@@ -2,18 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static final int INF = 1000000009;
+
     public static int n, k;
-    public static int[] map = new int[100000];
-    public static int[][] max = new int[11][100001];
+    public static int[] map = new int[100001];
+    public static int[][] dp = new int[100001][11];
 
     public static void init() {
-        for(int i = 0; i <= k; i++) {
-            for(int j = 0; j <= n; j++) {
-                max[i][j] = Integer.MIN_VALUE;
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= k; j++) {
+                dp[i][j] = -INF;
             }
         }
-        max[0][0] = 0;
+
+        dp[0][0] = 0;
+        if(map[1] >= 0) dp[1][0] = map[1];
+        else dp[1][1] = map[1];
     }
+
 
     public static void main(String[] args) throws IOException{
         // Please write your code here.
@@ -26,39 +32,35 @@ public class Main {
         for(int i = 1; i <= n; i++) {
             map[i] = Integer.parseInt(st.nextToken());
         }
-
+        
+        
         init();
 
-        for(int i = 1; i <= n; i++) {
+        int ans = map[1];
+        for(int i = 2; i <= n; i++) {
+
             if(map[i] >= 0) {
-                max[0][i] = Math.max(map[i], max[0][i - 1] == Integer.MIN_VALUE ? Integer.MIN_VALUE : max[0][i - 1] + map[i]);
+                dp[i][0] = Math.max(dp[i - 1][0] + map[i], map[i]);
+                ans = Math.max(ans, map[i]);
 
                 for(int j = 1; j <= k; j++) {
-                    if(max[j][i - 1] != Integer.MIN_VALUE) {
-                        max[j][i] = max[j][i - 1] + map[i];
-                    }
+                    if(dp[i - 1][j] == -INF) continue;
+        
+                    dp[i][j] = dp[i - 1][j] + map[i];
+                    ans = Math.max(ans, dp[i][j]);
                 }
-            } else {
-                for(int j = 1; j <= k; j++) {
-                    int prev = max[j - 1][i - 1] == Integer.MIN_VALUE ? Integer.MIN_VALUE : max[j - 1][i - 1] + map[i];
+            }
+            else {
+                dp[i][1] = Math.max(dp[i - 1][0] + map[i], map[i]);
+                ans = Math.max(ans, dp[i][1]);
 
-                    if(j == 1) {
-                        max[1][i] = Math.max(map[i], prev);
-                    } else {
-                        max[j][i] = prev;
-                    }
+                for(int j = 2; j <= k; j++) {
+                    if(dp[i - 1][j - 1] == -INF) continue;
+                    dp[i][j] = dp[i - 1][j - 1] + map[i];
+                    ans = Math.max(ans, dp[i][j]);
                 }
             }
         }
-
-        int ans = Integer.MIN_VALUE;
-
-        for(int i = 0; i <= k; i++) {
-            for(int j = 1; j <= n; j++) {
-                ans = Math.max(max[i][j], ans);
-            }
-        }
-
         System.out.print(ans);
     }
 }
