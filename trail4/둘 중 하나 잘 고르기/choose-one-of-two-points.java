b461db0a157816987aea3d56ccaac4c2e0c834b7
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-08-08 15:28:52 +0900

    [Passed] 둘 중 하나 잘 고르기 (91ms, 9MB)

--- trail4/둘 중 하나 잘 고르기/choose-one-of-two-points.java
+++ trail4/둘 중 하나 잘 고르기/choose-one-of-two-points.java
@@ -1,56 +1,53 @@
 import java.io.*;
 import java.util.*;
 
+// Greedy
+// 2 * n 개의 빨간색 카드 중 n 개의 카드를 파란색으로 교체하는 로직
+// red - blue 내림차순으로 정렬 후 앞에서 red 카드 n 개 뒤에서 blue 카드 n개 뽑기
+class Card implements Comparable<Card>{
+    int red, blue;
+
+    public Card(int red, int blue) {
+        this.red = red;
+        this.blue = blue;
+    }
+
+    @Override
+    public int compareTo(Card card) { // 내림차순으로 정렬
+        return (card.red - card.blue) - (this.red - this.blue);
+    }
+}
+
 public class Main {
     public static int n;
-    public static int[] red = new int[201];
-    public static int[] blue = new int[201];
+    public static Card[] cards = new Card[201];
 
     public static int[][] dp = new int[201][201]; // red, blue
-
-    public static void init() {
-        dp[0][0] = 0;
-
-        for(int i = 1; i <= 2 * n; i++) {
-            for(int j = 1; j <= 2 * n; j++) {
-                dp[i][j] = -1;
-            }
-            dp[i][0] = red[i] + dp[i - 1][0];
-            dp[0][i] = blue[i] + dp[0][i - 1];
-        }        
-    }
 
     public static void main(String[] args) throws IOException{
         // Please write your code here.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
-        for(int i = 1; i <= 2 * n; i++) {
+        for(int i = 0; i < 2 * n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            red[i] = Integer.parseInt(st.nextToken());
-            blue[i] = Integer.parseInt(st.nextToken());
+            int red = Integer.parseInt(st.nextToken());
+            int blue = Integer.parseInt(st.nextToken());
+            cards[i] = new Card(red, blue);
         }
 
-        init();
-        
+        Arrays.sort(cards, 0, 2*n);
 
-        int ans = -1;
-        for(int i = 1; i <= 2 * n; i++) {
-            for(int j = 1; j <= 2 * n; j++) {
-                if(i > n || j > n) continue;
-                dp[i][j] = Math.max(dp[i - 1][j] + red[i + j], dp[i][j - 1] + blue[i + j]);
-                
-                if(i == n && j == n) ans = Math.max(dp[i][j], ans);
-            }
+        int answer = 0;
+
+        for(int i = 0; i < n; i++) {
+            answer += cards[i].red;
         }
 
-        // for(int i = 0; i <= 2*n; i++) {
-        //     for(int j = 0; j <= 2*n; j++) {
-        //         System.out.print(dp[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
+        for(int i = n; i < 2*n; i++) {
+            answer += cards[i].blue;
+        }
 
-        System.out.print(ans);
+        System.out.print(answer);
     }
 }
