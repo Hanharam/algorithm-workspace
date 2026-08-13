d75bf7aba49bb31b66a91c84ecc85a2ebce83b55
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-08-13 22:27:57 +0900

    [Passed] 적절하게 정수를 변경하기 (92ms, 9MB)

--- trail4/적절하게 정수를 변경하기/change-number-properly.java
+++ trail4/적절하게 정수를 변경하기/change-number-properly.java
@@ -2,57 +2,80 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int n, m;
-    public static int[] arr = new int[501];
+    public static final int INT_MIN = Integer.MIN_VALUE;
+    public static final int MAX_K = 4;
+    public static final int MAX_M = 100;
+    public static final int MAX_N = 500;
 
-    public static int[][][] dp = new int[501][5][101]; // 인덱스 순서, 들어갈 숫자, 다른 횟수 = 점수
+    // dp[i][j] :
+    // 마지막으로 놓은 블록의 끝 위치가 i이고
+    // 지금까지 놓은 블록의 수가 j개일 때
+    // 얻을 수 있는 최대 유사도
+    public static int[][] dp = new int[MAX_N + 1][MAX_M + 2];
+
+    public static int n, m;
+    public static int[] a = new int[MAX_N + 1];
+
+    public static void init() {
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= m + 1; j++) {
+                dp[i][j] = INT_MIN;
+            }
+        }
+
+        dp[0][0] = 0;
+    }
 
     public static void main(String[] args) throws IOException{
-        // Please write your code here.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            a[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 0; i <= n; i++) {
-            for(int j = 0; j <= 4; j++) {
-                for(int k = 0; k <= m; k++) {
-                    dp[i][j][k] = -1;
-                }
-            }
-        }
+        init();
 
-        for(int i = 1; i <= 4; i++) {
-            dp[0][i][0] = 0;
-        }
+        for(int i = 1; i <= n; i++) {
+            // 정확히 i번째 숫자를 마지막으로
+            // 그 동안 블록을 총 j개 사용했을 때
+            // 얻을 수 있는 최대 유사도를 계산
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 1; j <= 4; j++) {
-                for(int k = 0; k <= m; k++) {
-                    if(dp[i][j][k] < 0) continue;
+            // 가장 마지막으로 놓은 블록의 위치를 [l, i]라 했을 때
+            // 해당 구간에 전부 k로 채워진 블록을 사용한 경우를 고려
 
-                    for(int idx = 1; idx <= 4; idx++) {
-                        int val = 0;
-                        if(idx == arr[i + 1]) val = 1;
-                        if(idx == j) dp[i + 1][idx][k] = Math.max(dp[i + 1][idx][k], dp[i][j][k] + val);
+            for(int j = 1; j <= m + 1; j++) {
+                for(int k = 1; k <= MAX_K; k++) {
+                    int similarity = 0;
 
-                        else if(idx != j && k < m) dp[i + 1][idx][k + 1] = Math.max(dp[i + 1][idx][k + 1], dp[i][j][k] + val);
+                    for(int l = i; l >= 1; l--) {
+                        similarity += (a[l] == k ? 1 : 0);
+
+                        // [l, i] 구간에 전부 k로 채워진 블록을 하나 추가한 경우
+                        // 지금까지의 사용한 블록의 수가 j가 되기 위해서는
+                        // l - i번째까지 사용한 블록의 수가 j - 1이어야 하므로
+                        // dp[l - 1][j - 1]에
+                        // [l, i] 구간에 전부 k로 채워진 블록을 하나 추가했을 때
+                        // 얻을 수 있는 유사도를 더한 값을 비교해볼 수 있음
+                        dp[i][j] = Math.max(dp[i][j], dp[l - 1][j - 1] + similarity);
                     }
                 }
             }
         }
 
+        // n개의 숫자에 대해 전부 고려했을 때
+        // 사용한 블록의 수가 m + 1을 넘지 않는 경우 중
+        // 가장 높은 유사도를 얻을 수 있는 경우를 선택
+
         int ans = 0;
-        for(int i = 1; i <= 4; i++) {
-            for(int j = 0; j <= m; j++) {
-                ans = Math.max(dp[n][i][j], ans);
-            }
-        }
-        System.out.print(ans);
+
+        for(int j = 1; j <= m + 1; j++)
+            ans = Math.max(ans, dp[n][j]);
+
+        System.out.println(ans);
     }
 }
