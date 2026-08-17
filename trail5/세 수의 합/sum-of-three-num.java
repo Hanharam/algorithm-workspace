9ea2ebe93ded5b85b43d60c743f7ebca3006205c
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-08-17 17:18:34 +0900

    [Passed] 세 수의 합 (662ms, 77MB)

--- trail5/세 수의 합/sum-of-three-num.java
+++ trail5/세 수의 합/sum-of-three-num.java
@@ -10,25 +10,32 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
+        HashMap<Integer, Integer> m = new HashMap<>();
+
         int[] arr = new int[n];
 
+        int ans = 0;
+
+        // 각 숫자가 몇 번 나왔는 지 기록
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+
+            m.put(arr[i], m.getOrDefault(arr[i], 0) + 1);
         }
 
-        long ans = 0;
+        // 배열을 순회하며 쌍을 만들어줌
+        for(int i = 0; i < n; i++) {
+            // 이미 순회한 적이 있으면 숫자를 빼기
+            // 같은 조합 방지
+            if(!m.containsKey(arr[i]))
+                m.put(arr[i], - 1);
+            else
+                m.put(arr[i], m.get(arr[i]) - 1);
 
-        for (int i = 0; i < n; i++) {
-            HashMap<Integer, Integer> map = new HashMap<>();
-
-            // i < j < 현재 위치가 되도록 관리
-            for (int j = i + 1; j < n; j++) {
-                int need = k - arr[i] - arr[j];
-
-                ans += map.getOrDefault(need, 0);
-
-                map.put(arr[j], map.getOrDefault(arr[j], 0) + 1);
+            for(int j = 0; j < i; j++) {
+                if(m.containsKey(k - arr[i] - arr[j]))
+                    ans += m.get(k - arr[i] - arr[j]);
             }
         }
 
