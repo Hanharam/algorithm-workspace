599831700df124ba39cb07e9219bc73254057b4e
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-08-17 18:11:02 +0900

    [Passed] 자주 등장한 top K 숫자 (760ms, 27MB)

--- trail5/자주 등장한 top K 숫자/top-k-frequent-elements.java
+++ trail5/자주 등장한 top K 숫자/top-k-frequent-elements.java
@@ -1,63 +1,54 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static int k;
-    public static int[] topK;
-    public static int[] topKCnt;
+class Pair implements Comparable<Pair> {
+    int cnt, num;
 
-    public static void findK(int cnt, int num) {
-
-        for (int i = 0; i < k; i++) {
-            // 현재 위치보다 우선순위가 높은 경우
-            if (cnt > topKCnt[i] ||
-                (cnt == topKCnt[i] && num > topK[i])) {
-
-                // 뒤에 있는 원소들을 한 칸씩 밀기
-                for (int j = k - 1; j > i; j--) {
-                    topKCnt[j] = topKCnt[j - 1];
-                    topK[j] = topK[j - 1];
-                }
-
-                topKCnt[i] = cnt;
-                topK[i] = num;
-
-                break;
-            }
-        }
+    public Pair(int cnt, int num) {
+        this.cnt = cnt;
+        this.num = num;
     }
+
+    @Override
+    public int compareTo(Pair b) {
+        if(cnt != b.cnt) return cnt - b.cnt;
+        return num - b.num;
+    }
+}
+
+public class Main {
+    public static final int MAX_N = 100000;
+
+    public static int n, k;
+    public static int[] arr = new int[MAX_N];
+    public static HashMap<Integer, Integer> freq = new HashMap<>();
+    public static ArrayList<Pair> v = new ArrayList<>();
+
 
     public static void main(String[] args) throws IOException{
         // Please write your code here.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        topK = new int[k];
-        topKCnt = new int[k];
-
         int[] arr = new int[n];
-        HashMap<Integer, Integer> m = new HashMap<>();
-
 
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            m.put(arr[i], m.getOrDefault(arr[i], 0) + 1);
+            freq.put(arr[i], freq.getOrDefault(arr[i], 0) + 1);
         }
 
-        for(Map.Entry<Integer, Integer> entry : m.entrySet()) {
-            int number = entry.getKey();
-            int count = entry.getValue();
-
-            findK(count, number);
+        for(Integer key : freq.keySet()) {
+            v.add(new Pair(freq.get(key), key));
         }
 
-        for(int num : topK) {
-            System.out.print(num + " ");
-        }
+        Collections.sort(v);
+
+        for(int i = (int)v.size() - 1; i >= (int)v.size() - k; i--)
+            System.out.print(v.get(i).num + " ");
 
     }
 }
