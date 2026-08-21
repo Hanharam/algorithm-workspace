7f12d2eb1a3dca9a96fcc2c34ddcae5367388db2
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-08-21 18:14:04 +0900

    [Passed] 최대로 연속한 숫자 (1708ms, 38MB)

--- trail5/최대로 연속한 숫자/maximum-consecutive-number.java
+++ trail5/최대로 연속한 숫자/maximum-consecutive-number.java
@@ -1,56 +1,55 @@
 import java.util.*;
 import java.io.*;
 
+class Tuple implements Comparable<Tuple> {
+    int len, s, e;
+
+    public Tuple(int len, int s, int e) {
+        this.len = len;
+        this.s = s;
+        this.e = e;
+    }
+
+    @Override
+    public int compareTo(Tuple t) {
+        if(len != t.len) return t.len - this.len;
+        else if(s != t.s) return s - t.s;
+        else return e - t.e;
+    }
+}
+
 public class Main {
-
-    public static void add(TreeMap<Integer, Integer> length, int n) {
-        if(n <= 0) return;
-        length.put(n, length.getOrDefault(n, 0) + 1);
-    }
-
-    public static void remove(TreeMap<Integer, Integer> length, int n) {
-        if(n <= 0) return;
-
-        int cnt = length.get(n);
-
-        if(cnt == 1) length.remove(n);
-        else length.put(n, cnt - 1);
-    }
+    public static int n, m;
+    public static TreeSet<Integer> sNum = new TreeSet<>();
+    public static TreeSet<Tuple> sLen = new TreeSet<>();
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        TreeSet<Integer> removed = new TreeSet<>();
-        TreeMap<Integer, Integer> length = new TreeMap<>();
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        removed.add(-1);
-        removed.add(n + 1);
+        sNum.add(-1);
+        sNum.add(n + 1);
 
-        add(length, n + 1);
+        sLen.add(new Tuple(n + 1, -1, n + 1));
 
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < m; i++) {
             int num = Integer.parseInt(st.nextToken());
 
-            int left = removed.lower(num);
-            int right = removed.higher(num);
+            sNum.add(num);
 
-            int old = right - left - 1;
-            remove(length, old);
 
-            add(length, right - num - 1);
-            add(length, num - left - 1);
+            int right = sNum.higher(num);
+            int left = sNum.lower(num);
 
-            removed.add(num);
+            sLen.remove(new Tuple(right - left - 1, left, right));
+            sLen.add(new Tuple(right - num - 1, num, right));
+            sLen.add(new Tuple(num - left - 1, left, num));
 
-            if(length.isEmpty()) sb.append(0).append("\n");
-            else sb.append(length.lastKey()).append("\n");
+            System.out.println(sLen.first().len);
         }
-        System.out.print(sb);
     }
 }
