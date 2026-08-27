90402a1a342296d92c6383017bf9a4e0bfb252cd
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-08-27 15:58:24 +0900

    [Passed] 연속한 K개의 숫자 (101ms, 9MB)

--- trail5/연속한 K개의 숫자/k-numbers-in-a-row.java
+++ trail5/연속한 K개의 숫자/k-numbers-in-a-row.java
@@ -2,6 +2,18 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 100000;
+
+    public static int n, k, b;
+    public static int[] arr = new int[MAX_N + 1];
+    public static int[] prefixSum = new int[MAX_N + 1];
+    public static int ans = INT_MAX;
+
+    public static int getSum(int s, int e) {
+        return prefixSum[e] - prefixSum[s - 1];
+    }
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -10,23 +22,18 @@ public class Main {
         int k = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        boolean[] arr = new boolean[n + 1];
-        int[] prefixSum = new int[n + 1];
-
-        Arrays.fill(arr, false);
-        for(int i = 0; i < b; i++) {
-            int num = Integer.parseInt(br.readLine());
-            arr[num] = true;
+        while(b-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+            arr[x] = 1;
         }
 
-        int ans = Integer.MAX_VALUE;
-        for(int i = 1; i + k - 1 <= n; i++) {
-            int cnt = 0;
-            for(int j = 0; j < k; j++) {
-                if(arr[i + j]) cnt++;
-            }
-            ans = Math.min(ans, cnt);
+        prefixSum[0] = 0;
+        for(int i = 1; i <= n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
         }
+
+        for(int i = 1; i <= n - k + 1; i++)
+            ans = Math.min(ans, getSum(i, i + k - 1));
 
         System.out.print(ans);
     }
