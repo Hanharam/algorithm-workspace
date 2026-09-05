5769fa0205bffc651d934e836c1acb0e89ceea19
Han Haram <108014261+Hanharam@users.noreply.github.com>
2026-09-05 16:48:46 +0900

    [Passed] 가장 짧은 부분합 (170ms, 13MB)

--- trail5/가장 짧은 부분합/shortest-subtotal.java
+++ trail5/가장 짧은 부분합/shortest-subtotal.java
@@ -9,30 +9,31 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int s = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
+        int[] arr = new int[n + 1];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
+        for(int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         int ans = Integer.MAX_VALUE;
-        int sum = 0;
-        int j = 0;
-        for(int i = 0; i < n; i++) {
-            
-            while(j < n) {
-                if(sum >= s) {
-                    ans = Math.min(ans, j - i);
-                    break;
-                }
 
-                sum += arr[j];
+        int sumVal = 0;
+        int j = 0;
+        for(int i = 1; i <= n; i++) {
+            
+            while(j + 1 <= n && sumVal < s) {
+                sumVal += arr[j + 1];
                 j++;
             }
-            sum -= arr[i];
+
+            if(sumVal < s) break;
+
+            ans = Math.min(ans, j - i + 1);
+            sumVal -= arr[i];
         }
-        if(ans == Integer.MAX_VALUE) System.out.print(-1);
-        else System.out.print(ans);
+
+        if(ans == Integer.MAX_VALUE) ans = -1;
+        System.out.print(ans);
     }
 }
